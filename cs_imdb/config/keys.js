acb2f133418128363b2851c21a53648d873d698b
Temi Aremu <temiloluwa.aremu@ontariotechu.net>
2020-07-15 15:13:24 -0400

    Update keys.js

--- cs_imdb/config/keys.js
+++ cs_imdb/config/keys.js
@@ -1,5 +1,8 @@
 dbPassword = 'mongodb+srv://admin:admin@movie-xtpb4.mongodb.net/test?retryWrites=true&w=majority'
 
 module.exports = {
-    mongoURI: dbPassword
+    mongoURI: dbPassword,
+      devServer: {
+        disableHostCheck: true
+    }
 };
